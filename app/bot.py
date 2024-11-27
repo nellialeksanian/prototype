75e8e0d93bfc8d0140f0026324c3d204f11126dd
Nelly Aleksanian <n.aleksanian.sas@gmail.com>
2024-11-27 15:40:46 +0300

    questions fixed

--- app/bot.py
+++ app/bot.py
@@ -9,7 +9,6 @@ from telegram.ext import (
     ContextTypes
 )
 from generation.generation_route import generate_route
-from embeddings.embeddings_similarity import search
 from generation.generate_artwork_info import generate_artwork_info
 from generation.generate_answer import generate_answer
 from dotenv import load_dotenv
@@ -29,71 +28,51 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle_user_input(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.user_data.get('state') == 'route_mode':
         user_query = update.message.text
-        # Use the external function to generate the route and artwork details
+        #generating the route and artwork details
         route_and_artworks, context.user_data['artworks'] = generate_route(user_query, k=10)
-        
-        # Send the generated route to the user
+        # send the generated route to the user
         await update.message.reply_text(route_and_artworks)
 
-        # Ask if the user is ready to start the journey
-        keyboard = [[InlineKeyboardButton("Да, я готов(а)", callback_data='start_journey')]]
+        # ask if the user is ready to start the journey
+        keyboard = [[InlineKeyboardButton("Да, я готов(а)", callback_data='next_artwork')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.message.reply_text("Вы готовы начать экскурсию?", reply_markup=reply_markup)
+
     elif context.user_data.get('state') == 'question_mode':
-        context.user_data['current_artwork_index'] -= 1
+
         user_question = update.message.text
-        current_artwork_index = context.user_data['current_artwork_index']
-        # Handle normal user question input
-        await update.message.reply_text(generate_answer(user_question, context.user_data['artworks'][current_artwork_index]))
+        last_shown_artwork_index = context.user_data['last_shown_artwork_index']
 
-# Function to start the journey and provide the first artwork information
-async def start_journey(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
+        await update.message.reply_text(generate_answer(user_question, context.user_data['artworks'][last_shown_artwork_index]))
 
-    # Switch user state to "question mode"
-    context.user_data['state'] = 'question_mode'
-    # Placeholder for the first artwork info, assuming this data is in the generated route
-    artwork_info = generate_artwork_info(context.user_data['artworks'][0])
-    context.user_data['current_artwork_index'] += 1
-
-    await query.edit_message_text(text=artwork_info)
-
-    # Ask if the user has questions
-    await query.message.reply_text("У вас есть вопросы о первой картине? Напишите ваш вопрос или нажмите 'Следующая картина'.")
-
-    # Next artwork button
-    keyboard = [[InlineKeyboardButton("Следующая картина", callback_data='next_artwork')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.message.reply_text("Нажмите ниже чтобы перейти к следующей картине.", reply_markup=reply_markup)
 
 async def next_artwork(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
     query = update.callback_query
-    current_artwork_index = context.user_data['current_artwork_index'] 
-    
-    if current_artwork_index < len(context.user_data['artworks']) - 1:
+    current_artwork_index = context.user_data['current_artwork_index']
 
+    if current_artwork_index < len(context.user_data['artworks']):
         context.user_data['state'] = 'question_mode'
-    
-        await query.message.reply_text(generate_artwork_info(context.user_data['artworks'][current_artwork_index]))
-        current_artwork_index += 1 
-        context.user_data['current_artwork_index'] = current_artwork_index
+        
+        context.user_data['last_shown_artwork_index'] = current_artwork_index
 
-        query = update.callback_query
         await query.answer()
+        await query.message.reply_text(generate_artwork_info(context.user_data['artworks'][current_artwork_index]))
 
-        # Ask if the user has questions
-        await query.message.reply_text("У вас есть вопросы о следующей картине? Напишите ваш вопрос или нажмите 'Следующая картина'.")
+        context.user_data['current_artwork_index'] += 1
 
-        # Option for the next artwork
-        keyboard = [[InlineKeyboardButton("Следующая картина", callback_data='next_artwork')]]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.message.reply_text("Нажмите ниже чтобы перейти к следующей картине.", reply_markup=reply_markup)
+        if context.user_data['current_artwork_index'] < len(context.user_data['artworks']):
+            keyboard = [[InlineKeyboardButton("Следующая картина", callback_data='next_artwork')]]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            await query.message.reply_text(
+                "Задайте вопрос о текущей картине или нажмите ниже, чтобы перейти к следующей.",
+                reply_markup=reply_markup
+            )
+        else:
+            await query.message.reply_text("Задайте вопрос о текущей картине. Это последняя картина нашего маршрута!")
     else:
-        await query.message.reply_text("Маршрут окончен. Надеюсь вам понравилось!")
+        await query.answer("Это была последняя картина.")
 
-# Main function to set up the bot
+
 def main():
     token = os.getenv("TELEGRAM_TOKEN")
     # Create the Application with the bot token
@@ -104,16 +83,10 @@ def main():
     
     # Register message handler for initial user input (generating route)
     app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^(?!.*Следующая картина).*$") & ~filters.COMMAND, handle_user_input))
-    
-    # Register callback query handler for starting the journey
-    app.add_handler(CallbackQueryHandler(start_journey, pattern='start_journey'))
 
     # Register callback query handler for next artwork
     app.add_handler(CallbackQueryHandler(next_artwork, pattern='next_artwork'))
-    
-    # Register the message handler for user questions ONLY after the user starts the journey
-    # app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_user_question))
-    
+  
     # Print a message to the console to confirm the bot is running
     print("Museum Guide Bot is ready and running!")
 
