d736aca06af8e299f5425b9d0fe274724cea1a6e
Angelina <a.khapava.sas@gmail.com>
2025-04-03 12:15:26 +0300

    delited run

--- app/generation/generation_route.py
+++ app/generation/generation_route.py
@@ -1,7 +1,6 @@
 import os
 from langchain_gigachat.chat_models import GigaChat
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import LLMChain
 from embeddings.embeddings_similarity import search
 from process_data.load_data import clean_text
 
