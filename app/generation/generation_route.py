f3a092c265ec10d958cf721d318c7146a503281a
Angelina <a.khapava.sas@gmail.com>
2025-04-03 12:14:33 +0300

    fix/ load_dotenv()

--- app/generation/generation_route.py
+++ app/generation/generation_route.py
@@ -3,6 +3,8 @@ from langchain_gigachat.chat_models import GigaChat
 from langchain_core.prompts import ChatPromptTemplate
 from embeddings.embeddings_similarity import search
 from process_data.load_data import clean_text
+from dotenv import load_dotenv
+load_dotenv()
 
 gigachat_token = os.getenv("GIGACHAT_TOKEN")
 
