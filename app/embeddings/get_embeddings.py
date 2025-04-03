b8b517b439587ab72823b74eaece276c2b29d005
Angelina <a.khapava.sas@gmail.com>
2025-04-03 11:22:44 +0300

    fix/  loading model with and without hf

--- app/embeddings/get_embeddings.py
+++ app/embeddings/get_embeddings.py
@@ -1,32 +1,16 @@
 from sentence_transformers import SentenceTransformer
-from transformers import AutoModel, AutoTokenizer
-# from datasets import Dataset
-# import pandas as pd
-# import torch
+import os
 
-# df = pd.read_csv('data/paintings_data_tables/dataset.csv')
-# df = pd.read_parquet('data/paintings_data_tables/Slovcova.parquet')
-# data = Dataset.from_pandas(df)
-model_name = "BAAI/bge-m3"
-# model_emb = SentenceTransformer("BAAI/bge-m3"
-model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
 try:
-    model_emb = SentenceTransformer(modules=[model])
-    print(model_emb)
-except:
-    print('error')
-print('Модель  загружена')
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# model_emb.to(device)
+    model_emb = SentenceTransformer("BAAI/bge-m3")
+except Exception as e:
+    MODEL_PATH = os.getenv("MODEL_PATH")
+    print("Error loading model:", e)
+    print("Trying to load from local path...")
+    model_emb = SentenceTransformer(MODEL_PATH)
 
 def get_embeddings(text_list):
     embeddings = model_emb.encode(
-        text_list, convert_to_tensor=True, #device=device
+        text_list, convert_to_tensor=True, 
     )
     return embeddings
-
-# embeddings_dataset = data.map(
-#     lambda x:{"embeddings": get_embeddings(x["text"]).cpu().numpy()}
-# )
-
-# embeddings_dataset.to_parquet('data/paintings_data_tables/Slovcova_embeddings.parquet') 
