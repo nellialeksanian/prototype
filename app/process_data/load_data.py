d63b2f2ddf4c6c245d3cf68f20285f7c00e4a2cd
Angelina <a.khapava.sas@gmail.com>
2025-04-14 12:20:49 +0300

    fixes with data

--- app/process_data/load_data.py
+++ app/process_data/load_data.py
@@ -3,7 +3,7 @@ from datasets import Dataset
 import re
 
 def load_data():
-    df_loaded = pd.read_parquet('data/Museum/museum_embeddings.parquet')
+    df_loaded = pd.read_parquet('data/Slovcova/Slovcova_embeddings_map_img.parquet')
     embeddings_dataset = Dataset.from_pandas(df_loaded)
     embeddings_dataset.add_faiss_index(column="embeddings")
 
